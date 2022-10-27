661fd283004ec1b97429b7b6cc2e6998280c0a63
IgnaDev <arcortoon@gmail.com>
2022-10-27 10:54:58 -0300

    Validamos el error y el loading

--- src/UseState.js
+++ src/UseState.js
@@ -1,30 +1,48 @@
 import React from 'react'
 
+const SECURITY_CODE = 'paradigma';
+
 export function UseState({ name }) {
-    const [error, setError] = React.useState(true)
+    const [value, setValue] = React.useState("")
+    const [error, setError] = React.useState(false)
     const [loading, setLoading] = React.useState(false)
+
+
+
     React.useEffect(()=>{
         console.log("Comenzando el efecto")
         if(loading){
             setTimeout(()=>{
-                console.log("Comenzando la validacion")
-                setLoading(false)
+                if(value===SECURITY_CODE){
+                    setLoading(false)
+                }else{
+                    setLoading(false)
+                    setError(true)
+                }
                 console.log("Terminando la validacion")
             }, 3000)
         }
         console.log("Terminando el efecto")
     },[loading])
+
     return (
         <div>
         <h2>Eliminar {name}</h2>
         <p>Por favor, escriba el código de seguridad</p>
-        {error && (
+        {(error && !loading) && (
             <p>Ta todo mal</p>
         )}
         {loading && (
             <p>Cargando...</p>
         )}
-        <input type='text' placeholder='código de seguridad'/>
+        <input 
+        type='text' 
+        placeholder='código de seguridad'
+        value={value}
+        onChange={(event)=>{
+            setValue(event.target.value)
+        }}
+        />
         <button onClick={
             ()=> setLoading(true)
         }>Comprobar</button>
